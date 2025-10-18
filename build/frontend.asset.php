8ffb11cf839940bfb500ceea4b0062f8c66df01d
coderz-studio <coderzstudio8@gmail.com>
2025-10-19 01:37:18 +0530

    remove build folder

--- build/frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'aa4f4fc23736ac8058a6');
--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '1cd1b559679692bfcba6');
